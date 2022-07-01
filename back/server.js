e6c6dfb92714b8a3965eb1f5af245a60e5ff6763
nono <renaud.sannier@laposte.net>
2022-07-01 22:38:06 +0200

    Update server.js

--- back/server.js
+++ back/server.js
@@ -1,6 +1,11 @@
-var app = require('./app')
-var http = require('http').Server(app);
-
-http.listen(function(){
-  console.log('listening');
+var app = require('./app')
+
+var http = require('http').Server(app);
+
+//tttt
+
+http.listen(function(){
+
+  console.log('listening');
+
 })
